c7a0e343dddb0038edb72a6550b88ec7a95a72df
Brandur <brandur@mutelight.org>
2013-05-27 11:49:51 -0700

    Use secure version of these resources

--- lib/brandur_org/books.rb
+++ lib/brandur_org/books.rb
@@ -1,7 +1,7 @@
 module BrandurOrg
   class Books < Sinatra::Base
     get "/books" do
-      redirect to("http://metrics.brandur.org/books")
+      redirect to("https://metrics.brandur.org/books")
     end
   end
 end
