99dc8b29a088123b4ad2622ab09a843db0e12052
Brandur <brandur@mutelight.org>
2013-09-01 09:04:28 -0700

    Change publishing time

--- lib/org/articles/accessible-apis.rb
+++ lib/org/articles/accessible-apis.rb
@@ -5,7 +5,7 @@ module Org
 A set of patterns to make APIs more accessible to developers; lowering the barrier of entry for new users, and easing the maintenance of consuming applications.
       eos
       location:     "San Francisco",
-      published_at: Time.parse("Sun Sep  1 08:56:44 PDT 2013"),
+      published_at: Time.parse("Sun Sep  1 08:59:44 PDT 2013"),
       signature:    true,
       title:        "Developer Accessible APIs",
     } do
