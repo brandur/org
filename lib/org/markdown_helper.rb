710e976ef9ec88ae5c0e27ba8c26661013f66588
Brandur <brandur@mutelight.org>
2013-09-29 13:12:26 -0700

    Use MarkdownHelper

--- lib/org/markdown_helper.rb
+++ lib/org/markdown_helper.rb
@@ -1,10 +1,30 @@
-module MarkdownHelper
-  def self.render(str)
-    renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-      fenced_code_blocks: true, hard_wrap: true)
+module Org
+  module MarkdownHelper
+    def self.render(str)
+      if RUBY_PLATFORM == 'java'
+        render_kramdown(str)
+      else
+        render_redcarpet(str)
+      end
+    end
 
-    # Redcarpet now allows a new renderer to be defined. This would be better.
-    renderer.render(str).
-      gsub /<code class="(\w+)">/, %q|<code class="language-\1">|
+    private
+
+    def self.render_kramdown(str)
+      Kramdown::Document.new(str, input: 'GFM').to_html
+    end
+
+    def self.render_redcarpet(str)
+      renderer = Redcarpet::Markdown.new(
+        Redcarpet::Render::HTML.new({
+          with_toc_data: true
+        }),
+        Slim::Embedded.default_options[:markdown]
+      )
+
+      # Redcarpet now allows a new renderer to be defined. This would be better.
+      renderer.render(str).
+        gsub /<code class="(\w+)">/, %q|<code class="language-\1">|
+    end
   end
 end
