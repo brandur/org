fea76fee3893d9957016a0c4dd83791bc87fb7c1
Brandur <brandur@mutelight.org>
2014-01-26 10:14:05 -0800

    Improvements to page article

--- lib/org/drafts/page.rb
+++ lib/org/drafts/page.rb
@@ -9,6 +9,9 @@ How the page almost transitioned successfully to the digital world, but is in de
       published_at: Time.parse("Tue Dec 24 07:54:44 PDT 2013"),
       signature:    true,
       title:        "Lamenting the Death of the Page",
+      attributions: <<-eos
+Header image by <strong><a href="https://www.flickr.com/photos/67499195@N00/717747166">Andreas Levers</a></strong>. Licensed under Creative Commons BY-NC 2.0.
+      eos
     } do
       render_article do
         slim :"articles/signature", layout: !pjax?
