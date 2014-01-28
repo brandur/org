e87fd537f30f1500c645f26cc5d99a13527aa82f
Brandur <brandur@mutelight.org>
2014-01-28 14:07:17 -0800

    Move Twitter into project

--- lib/org/twitter.rb
+++ lib/org/twitter.rb
@@ -1,7 +1,25 @@
 module Org
   class Twitter < Sinatra::Base
+    helpers Helpers::Common
+    helpers Helpers::Twitter
+
+    configure do
+      set :views, Config.root + "/views"
+    end
+
+    before do
+      log :access_info, pjax: pjax?
+      cache_control :public, :must_revalidate, max_age: 3600
+    end
+
     get "/twitter" do
-      redirect to("https://metrics.brandur.org/twitter")
+      @tweets = DB[:events].reverse_order(:occurred_at).filter(type: "twitter").
+        filter("metadata -> 'reply' = 'false'")
+      last_modified(@tweets[0][:occurred_at]) if Config.production?
+      @tweets = @tweets.all
+      @tweets = group_by_month_and_year(@tweets)
+      @title = "Twitter"
+      slim :"twitter/index"
     end
   end
 end
