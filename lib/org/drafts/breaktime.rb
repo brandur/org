776d1d4e589544a1fe94695a8a0f336cbe16ede4
Brandur <brandur@mutelight.org>
2014-01-31 09:08:37 -0800

    Add hook

--- lib/org/drafts/breaktime.rb
+++ lib/org/drafts/breaktime.rb
@@ -1,8 +1,9 @@
 module Org
   class Articles
     article "/breaktime", {
-    # hook:         <<-eos,
-    # eos
+      hook:         <<-eos,
+In search for (and the discovery of) an alternative to BreakTime.
+      eos
       location:     "San Francisco",
       published_at: Time.parse("Thu Jan 30 22:15:17 PST 2014"),
       signature:    true,
