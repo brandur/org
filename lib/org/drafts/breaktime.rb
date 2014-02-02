8ae70869871950c3d4d6dfd74a376880199849e3
Brandur <brandur@mutelight.org>
2014-02-02 10:15:39 -0800

    Update BreakTime's publish date

--- lib/org/drafts/breaktime.rb
+++ lib/org/drafts/breaktime.rb
@@ -5,7 +5,7 @@ module Org
 In search for (and the discovery of) an alternative to BreakTime.
       eos
       location:     "San Francisco",
-      published_at: Time.parse("Thu Jan 30 22:15:17 PST 2014"),
+      published_at: Time.parse("Sun Feb  2 10:15:28 PST 2014"),
       signature:    true,
       title:        "BreakTime Classic",
     } do
