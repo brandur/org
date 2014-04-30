412e0473d52f44fe69ec80e65ff080787d991870
Brandur <brandur@mutelight.org>
2014-04-30 10:39:31 -0700

    Slightly more approachable title

--- lib/org/articles/schema-stubs.rb
+++ lib/org/articles/schema-stubs.rb
@@ -7,7 +7,7 @@ Stubbing distributed services with Sinatra-based services stubs and enabling str
       location:     "San Francisco",
       published_at: Time.parse("Wed Apr 30 09:23:22 PDT 2014"),
       signature:    true,
-      title:        "Schema Stubs",
+      title:        "Testing Distributed Services with JSON Schema",
     } do
       render_article do
         slim :"articles/signature", layout: !pjax?
