6f6c6bf9c4806805322c3d8b10645f6bac939498
Brandur <brandur@mutelight.org>
2014-04-30 09:23:03 -0700

    Publish "Schema Stubs"

--- lib/org/articles/schema-stubs.rb
+++ lib/org/articles/schema-stubs.rb
@@ -5,7 +5,7 @@ module Org
 Stubbing distributed services with Sinatra-based services stubs and enabling stronger constraints with JSON Schema.
       eos
       location:     "San Francisco",
-      published_at: Time.parse("Wed Apr 30 08:25:22 PDT 2014"),
+      published_at: Time.parse("Wed Apr 30 09:23:22 PDT 2014"),
       signature:    true,
       title:        "Schema Stubs",
     } do
