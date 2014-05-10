f8a52b273c2c9fbc7c15f7425ad7df034950bf47
Brandur <brandur@mutelight.org>
2014-05-10 15:32:25 +0200

    Implemented list page for fragments

--- lib/org/fragments.rb
+++ lib/org/fragments.rb
@@ -16,10 +16,43 @@ module Org
 
     namespace "/fragments" do
       get do
+        @title = "Fragments"
+        @fragments = Fragments.fragments
+        slim :"fragments/index", layout: !pjax?
       end
 
       get "/modern" do
         slim :"fragments/show", layout: !pjax?
+      end
+    end
+
+    private
+
+    def self.fragments
+      # not threadsafe, but it doesn't matter
+      @@fragments ||= begin
+        fragment_data = Dir[Config.root + "/fragments/**/*.md"].map { |f|
+          contents = File.read(f)
+          if contents =~ /\A(---\n.*?\n---)\n(.*)\Z/m
+            meta = YAML.load($1)
+            {
+              content:      $2,
+              published_at: meta["published_at"],
+              slug:         File.basename(f).chomp(".md"),
+              title:        meta["title"],
+            }
+          else
+            raise "No YAML front matter for #{f}."
+          end
+        }.sort_by { |f| f[:published_at] }.reverse
+
+        # take advantage of knowing about ordered hashes in Ruby to make sure
+        # that these stay in the right order
+        fragments = {}
+        fragment_data.each do |fragment|
+          fragments[fragment[:slug]] = fragment
+        end
+        fragments
       end
     end
   end
