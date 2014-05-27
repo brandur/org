5b544a3245a74afbce6dd4a917d79ec4b101145b
Brandur <brandur@mutelight.org>
2014-05-27 15:28:25 +0200

    Update publish date

--- lib/org/drafts/elegant-apis.rb
+++ lib/org/drafts/elegant-apis.rb
@@ -5,7 +5,7 @@ module Org
 A quick tour through some of the fundamentals of JSON Schema and Hyper-schema, and how they can be applied to building APIs.
       eos
       location:     "Berlin",
-      published_at: Time.parse("Mon May 26 09:26:59 CEST 2014"),
+      published_at: Time.parse("Tue May 27 15:28:14 CEST 2014"),
       signature:    true,
       title:        "Elegant APIs with JSON Schema",
     } do
