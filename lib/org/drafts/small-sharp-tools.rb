7baf8af4107352116b475e375bc4d90d94a3bcf1
Brandur <brandur@mutelight.org>
2014-12-12 09:20:58 -0800

    Editing + date bump on Small, Sharp, Tools

--- lib/org/drafts/small-sharp-tools.rb
+++ lib/org/drafts/small-sharp-tools.rb
@@ -2,10 +2,10 @@ module Org
   class Articles
     article "/small-sharp-tools", {
       hook:         <<-eos,
-A few words on the Unix philosophy of building small programs that do one thing well, and compose for more expansive functionality.
+A few words on the Unix philosophy of building small programs that do one thing well, and compose for comprehensive functionality.
       eos
       location:     "San Francisco",
-      published_at: Time.parse("2014-09-28T01:41:11Z"),
+      published_at: Time.parse("2014-12-12T17:20:30Z"),
       signature:    true,
       title:        "Small, Sharp Tools",
     } do
