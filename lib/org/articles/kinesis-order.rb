639f9e79c711fd1d17f28eba10dfbd73094c30e1
Brandur <brandur@mutelight.org>
2015-03-04 17:19:08 -0800

    Publish article on Kinesis ordering

--- lib/org/articles/kinesis-order.rb
+++ lib/org/articles/kinesis-order.rb
@@ -1,13 +1,13 @@
 module Org
   class Articles
-    article "/order", {
+    article "/kinesis-order", {
       hook:         <<-eos,
 On guaranteeing order with the bulk put API of an event stream.
       eos
       location:     "San Francisco",
-      published_at: Time.parse("2015-03-02T16:09:27Z"),
+      published_at: Time.parse("2015-03-05T01:13:46Z"),
       signature:    true,
-      title:        "Guaranteeing Order with Bulk Puts",
+      title:        "Guaranteeing Order with Kinesis Bulk Puts",
     } do
       render_article do
         slim :"articles/signature", layout: !pjax?
