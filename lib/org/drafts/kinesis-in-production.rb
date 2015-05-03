7b21231cde751ffb38b208a28d85b6c89bdad8da
Brandur <brandur@mutelight.org>
2015-05-03 14:09:30 -0700

    Change title and add image assets

--- lib/org/drafts/kinesis-in-production.rb
+++ lib/org/drafts/kinesis-in-production.rb
@@ -1,13 +1,13 @@
 module Org
   class Articles
-    article "/kinesis-in-retrospect", {
+    article "/kinesis-in-production", {
       hook:         <<-eos,
 A short write-up on findings, limitations, and opinion on Kinesis after a month in production.
       eos
       location:     "San Francisco",
       published_at: Time.parse("2015-05-03T17:35:37Z"),
       signature:    true,
-      title:        "Kinesis in Retrospect",
+      title:        "A Month of Kinesis in Production",
     } do
       render_article do
         slim :"articles/signature", layout: !pjax?
