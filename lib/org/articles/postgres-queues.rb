b5a8b4a184ab1abddab711f43c88361681465cd7
Brandur <brandur@mutelight.org>
2015-05-18 16:17:24 -0700

    Improve hook

--- lib/org/articles/postgres-queues.rb
+++ lib/org/articles/postgres-queues.rb
@@ -2,7 +2,7 @@ module Org
   class Articles
     article "/postgres-queues", {
       hook:         <<-eos,
-How long-lived transactions can cause serious trouble for hot tables due to the way that Postgres' concurrency model is implemented.
+How Postgres' concurrency model coupled with long-lived transactions can degrade the performance of indexes on hot tables in your database.
       eos
       location:     "San Francisco",
       published_at: Time.parse("2015-05-18T23:13:23Z"),
