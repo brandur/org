7a8d324dc2376e7d4709fac9355c90ee92baf656
Brandur <brandur@mutelight.org>
2015-08-13 20:37:05 -0700

    Commit header

---
+++ lib/org/drafts/ed25519.rb
@@ -0,0 +1,17 @@
+module Org
+  class Articles
+    article "/ed25519", {
+      hook:         <<-eos,
+Building internal authorization infrastructure based on the fast Ed25519 public key algorithm.
+      eos
+      location:     "San Francisco",
+      published_at: Time.parse("2015-08-12T16:46:42Z"),
+      signature:    true,
+      title:        "Ed25519",
+    } do
+      render_article do
+        slim :"articles/signature", layout: !pjax?
+      end
+    end
+  end
+end
