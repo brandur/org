75687bb103fec4380c0a18af93e14ef9ba4b46a8
Brandur <brandur@mutelight.org>
2015-09-14 16:24:01 -0700

    Modify title of article

--- lib/org/drafts/ed25519.rb
+++ lib/org/drafts/ed25519.rb
@@ -7,7 +7,7 @@ Building internal authorization infrastructure based on the fast Ed25519 public 
       location:     "San Francisco",
       published_at: Time.parse("2015-08-12T16:46:42Z"),
       signature:    true,
-      title:        "Ed25519",
+      title:        "Distributed Authorization With Ed25519",
     } do
       render_article do
         slim :"articles/signature", layout: !pjax?
