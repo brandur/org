efe21dcd578f8b05383122a3ac585d5ca28af639
Brandur <brandur@mutelight.org>
2015-11-04 22:24:11 -0800

    Remove redundant "in retrospect"

--- lib/org/articles/heroku-values.rb
+++ lib/org/articles/heroku-values.rb
@@ -2,7 +2,7 @@ module Org
   class Articles
     article "/heroku-values", {
       hook:         <<-eos,
-In retrospect, some of my favorite practices and ideas from almost four years at Heroku.
+Some of my favorite practices and ideas from almost four years at Heroku.
       eos
       image:        "/assets/heroku-values/heroku-values.jpg",
       location:     "San Francisco",
