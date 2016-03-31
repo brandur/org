6498cee7ea287f30ded23d4924f37cafc7b3564d
Brandur <brandur@mutelight.org>
2016-03-31 13:37:16 -0700

    Update timestamp

--- lib/org/drafts/go.rb
+++ lib/org/drafts/go.rb
@@ -5,7 +5,7 @@ module Org
 Notes on the language after spending a few weeks building a large project in it.
       eos
       location:     "San Francisco",
-      published_at: Time.parse("2016-03-10T05:32:08Z"),
+      published_at: Time.parse("2016-03-31T20:37:11Z"),
       signature:    true,
       title:        "Notes on Go",
     } do
