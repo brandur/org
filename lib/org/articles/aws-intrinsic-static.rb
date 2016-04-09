5cc2892754a21625ddeea502b39e26a6d7bac4d4
Brandur <brandur@mutelight.org>
2016-04-08 19:11:37 -0700

    Bump publish timestamp

--- lib/org/articles/aws-intrinsic-static.rb
+++ lib/org/articles/aws-intrinsic-static.rb
@@ -5,7 +5,7 @@ module Org
 Building a static site on AWS with a global CDN, free HTTPS with automatic certificate renewal, and a CI-based deployment process powered by GitHub pull requests.
       eos
       location:     "San Francisco",
-      published_at: Time.parse("2016-03-22T05:04:34Z"),
+      published_at: Time.parse("2016-04-09T02:11:19Z"),
       signature:    true,
       title:        "The Intrinsic Static Site",
     } do
